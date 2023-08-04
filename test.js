7d29e64fee42d05bb80e4a4f0dc30c04f3245788
longhoang <[hoalanh21112002@gmail.com]>
2023-08-04 09:50:15 +0700

    Init project

---
+++ test.js
@@ -0,0 +1,17 @@
+const obArr = [
+  {
+    name: 'long',
+    age: 19,
+  },
+  {
+    name: 'lanh',
+    age: 18,
+  },
+  {
+    name: 'loan',
+    age: 17,
+  },
+];
+
+console.log(obArr.splice(1, 1));
+console.log(obArr);
