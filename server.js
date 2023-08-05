8c6f58f02fa823e4aad762e266872519f6e7a251
longhoang <[hoalanh21112002@gmail.com]>
2023-08-05 21:48:43 +0700

    Schema and model, create schema and model with mongoose

--- server.js
+++ server.js
@@ -15,11 +15,6 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 ); //link connect to DBs
 
-//CONNECT WITH LOCAL DATABASE
-// mongoose
-//   .connect(process.env.DATABASE_LOCAl)
-//   .then(() => console.log('Connect DB success'));
-
 //CONNECT WITH HOSTED CLOUD DATABASE ALTAS
 mongoose
   .connect(DB)
@@ -28,9 +23,26 @@ mongoose
     console.log('DB connect successful');
   })
   .catch((err) => console.log(err));
-//mongoose.connect(link, option): return promise, you can set up some option{} but new mongoose up to 7 version so you don't need to set up some option to mongoose work stable, and maybe you can setup some options help you development DB
-//cuz promise return so you can use then() handle this promise, and use catch() method to catch the error if it occurs
 
+//>>>>>>CREATE SIMPLE SCHEMA
+// schema use to specify and decriber our data to set default, validation and so on...
+// we can set type of field, better we can set schema type option for each field or some specific field
+const tourSchema = new mongoose.Schema({
+  // name: String,
+  name: {
+    type: String,
+    required: [true, 'Tour name is required field'], // this is validator check name has data or not and message and in mongoose has a lot validator also we can create or own
+    unique: true, // the name must be unique
+  }, // this is schema type option
+  //[true, 'Tour name is required field'] you can have message when error use []
+  price: { type: Number, default: 4.5 },
+  //?the schema option type for each fields usally maybe similar but also it's can be not similar
+  rating: { type: Number, required: [true, 'Tour price is required field'] },
+});
+
+//CREATE MODEL
+//model name first letter is uppercase
+const Tour = mongoose.model('Tour', tourSchema); //create model based on schema
 //! 4,START SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
