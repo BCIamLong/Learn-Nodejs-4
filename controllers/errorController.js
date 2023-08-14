fca62f41c01257b3bcc1d754f85977db5eae9446
longhoang <[hoalanh21112002@gmail.com]>
2023-08-14 08:26:06 +0700

    Error Dev and Prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,19 +1,47 @@
-// ?WE ALSO WANNA CREATE ERRORS MIDDLE HANDLER IN HERE IN THE SAME FILE TO EASY TO MANAGE BECAUSE WHEN YOU PROJECT IS BIG YOU NEED MANY ERROR HANDLERS TO HANDLE MANY TYPES OF ERRORS AND THE CODE FOR THIS WILL REPEAT MANY TIMES
-//?HANDLERS IN MVC ALSO IS CONTROLLERS
-//--> so we will create errors controller to constain handlers errors methods
-//--! that's maybe litle weird because we don't have error resources, //?and this is only a way to implements errors handlers we also can create file errorHandlers as helpers,....
-//--! and in MVC error handlers look like controller so you can pick it in here but maybe we also have another way
+//!HANDLE ERROR WIHT DEVELOPMENT AND PRODUCTION ENVIROMENT: error handling mechanism(co che)
 
-//!IT LOOK LIKE PLUGIN CAN USE MANY TIMES IN YOUR PROJECT THAT'S USEFUL
-module.exports = (err, req, res, next) => {
-  //   console.error(err.stack);
-  // with four parameters express know this is error handling middleware, because express has error middleware handle out of the box
+//? In development we wanna get as much information that the error occursed as possible, and when the error back we will log it in console to see info error and fix it
+const sendErrorDev = (err, res) => {
   err.statusCode = err.statusCode || 500;
-  //* Why we need to set default: because there will be errors that are not comming from us, because there are gonna be errors withou status code so errors that not create by us but maybe some another place in node js apllication
   err.status = err.status || 'error';
   res.status(err.statusCode).json({
     status: err.status,
+    error: err,
     message: err.message,
+    stack: err.stack,
   });
-  next();
+  // next(); cuz this middleware function is a last one so we don't need use next()
+};
+
+//? In production we wanna leak as little information about our error to client as possible, for example we only send a nice friendly message for user know what’s wrong
+//--> production is important part so you need to be careful
+const sendErrorProd = (err, res) => {
+  //!operational errors: --> send to client
+  if (err.isOperational)
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+  //!programing errors: --> don't leak(tiet lo) error detail
+  //*1, log errors in hosted platform: heroku,... for the dev can watch what's going on here? => fix errors
+  //--!notice this console only log on the platform your project hosted(not for client)
+  console.error(`ERROR: ${err}`);
+
+  //?WE ALSO HAVE THE ERRORS COME FROM MONGOOSE WE DON'T MARK THAT'S IS OPEARATIONAL SO RIGHT KNOW WE WILL SEND SIMPLE GENERIC ERROR MESSAGE AS THIS CODE BELOW FOR EXAMPLE A VALIDATION ERRORS,...
+  //!BUT WE ALSO NEED TO MARK THEM OPERATIONAL AND SEND THE APPROPRITE(THICH HOP) ERROR MESSAGE BACK TO CLIENT FOR EXAMPLE: INPUT DATA INVALID WE CAN SEND MESSAGE FOR CLIENT,...
+  //*2,send generic error
+  res.status(500).json({
+    status: 'Error',
+    message: 'Something went wrong',
+  });
+
+  // next(); cuz this middleware function is a last one so we don't need use next()
+};
+
+module.exports = (err, req, res, next) => {
+  if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
+
+  //* --> AND WE WILL MARK MONGOOSE ERROR TO SEND THE APPROPRITE(THICH HOP) ERROR MESSAGE BACK TO CLIENT IN THE IF BLOCK BELOW
+  if (process.env.NODE_ENV === 'production') sendErrorProd(err, res);
 };
--- utils/appError.js
+++ utils/appError.js
@@ -1,15 +1,14 @@
+//! REMEMBER WE MARK ALL THE ERRORS THAT WE CREATE USING APPERROR AS IS OPERATIONAL SET TO TRUE => THAT'S THE OPERATIONAL ERRORS AND THESE ALL ERROR WE WILL SEND FOR CLIENT, AT LEAST IN PRODUCTION
+//?And with another errors as the programing error or unknown error as error with packages, libraries,... we don't send any error messages for client in production
+
 class AppError extends Error {
   constructor(statusCode, message) {
-    super(message); //to inherit message from Error class
+    super(message);
     this.statusCode = statusCode;
-    //we don't need pass status to constructor parameters because status depends on status code
     this.status = this.statusCode < 500 ? 'fails' : 'error';
-    this.isOperational = true; //we can use this property to check and only send error mesage back to the client for these operational errors that we created using this class
-    //-->this useful because some unexpected can occurs in your application for example programing error, some bugs in packages in our app
-    //--> we need this property to distinguish operational error with other errors to know actually we should handle what's operational errors
-    //--> example: if the programing error occurs and we send back for client so what's happen? the user don't know anything about code and they don't understand => we need to avoid this case
 
-    //!When create instance for this class we don't add this instance and constructor is called to create properties and methods don't appear in stack and will not pollute(o nhiem) stack trace
+    //* --> and isOperational will allow we do that in errors controller
+    this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
 }
